a158f53fb6c376a1a31c7b488daba67c120b0709
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-05 00:32:36 -0300

    refactor: change delete folder interfaces database

--- src/database/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './user-data/user-data-interface'
-export * from './product-data/product-data-interface'
-export * from './category-data/category-data-interface'
-export * from './cart-data/cart-data-interface'
--- src/database/interfaces/product-data/product-data-interface.ts
+++
@@ -1,6 +0,0 @@
-import { ProductRepository } from '@/domain/product'
-
-export interface IProductData {
-  getProductList(): Promise<ProductRepository[]>
-  getProductListByCategory(category: string): Promise<ProductRepository[]>
-}
