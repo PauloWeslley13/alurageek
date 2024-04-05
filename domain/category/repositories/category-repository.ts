1a7d351d731c39d10544b5f27050250391331b76
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-05 00:12:49 -0300

    refactor: change move file domain

--- domain/category/repositories/category-repository.ts
+++
@@ -1,9 +0,0 @@
-export class CategoryRepository {
-  public readonly id: string
-  public name: string
-
-  constructor(props: CategoryRepository) {
-    this.id = props.id
-    this.name = props.name
-  }
-}
