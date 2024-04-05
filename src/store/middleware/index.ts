3ddd36834998443ea3b81445f41ab3e3a6c7dff4
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-05 00:17:07 -0300

    refactor: change delete folder store

--- src/store/middleware/index.ts
+++
@@ -1,6 +0,0 @@
-// import { listenerCart } from './cart-listener/cart-listener'
-import { sagaMiddleware } from './saga-middleware/saga-middleware'
-
-export const middleware = [sagaMiddleware]
-
-export { sagaMiddleware }
