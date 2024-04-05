26c6179edad7b0c087e2b630bc7ac583f8eb1376
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-05 00:15:50 -0300

    refactor: change move folder pages

--- src/pages/panel-adm/modules/create-category/schema-category.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod'
-
-export const schemaCategory = z.object({
-  name: z.string().min(1, { message: 'Informe o nome da categoria' }),
-})
--- src/pages/products/modules/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './product-create/product-create'
-export * from './product-detail/product-detail'
-export * from './product-list-items/product-list-items'
-export * from './dialog-edit-product/dialog-edit-product'
