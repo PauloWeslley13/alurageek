9fad549e05a87ca526fd9fb4692e8f8e4942dd8f
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-07 10:21:05 -0300

    chore: change config jest and tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js.ts.jsx.tsx}'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
   },
